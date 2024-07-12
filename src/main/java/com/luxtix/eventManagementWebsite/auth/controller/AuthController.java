60d32a8f9150578293344e3d608ddd278df54c96
Hendry Tjahaja <80196576+akaslayer@users.noreply.github.com>
2024-07-12 21:11:10 +0700

    Development (#29)
     Fix(Auth):fix the dto response in the auth

--- src/main/java/com/luxtix/eventManagementWebsite/auth/controller/AuthController.java
+++ src/main/java/com/luxtix/eventManagementWebsite/auth/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.luxtix.eventManagementWebsite.auth.service.AuthService;
 import com.luxtix.eventManagementWebsite.response.Response;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -50,12 +52,14 @@ public class AuthController {
         log.info("Token requested for user :" + userDetails.getUsername() + " with roles: " + userDetails.getAuthorities().toArray()[0]);
         LoginResponseDto resp = authService.generateToken(authentication);
 
-        Cookie cookie = new Cookie("Sid", resp.getToken());
+
+        Cookie cookie = new Cookie("Sid", resp.getAccessToken());
         cookie.setMaxAge(6000);
         cookie.setPath("/");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Set-Cookie", cookie.getName() + "=" + cookie.getValue() + "; Path=/; HttpOnly");
-        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(Response.successfulResponse( "Login successfully", resp));
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(resp);
+
     }
 
     @PostMapping("/logout")
--- src/main/java/com/luxtix/eventManagementWebsite/auth/dto/LoginResponseDto.java
+++ src/main/java/com/luxtix/eventManagementWebsite/auth/dto/LoginResponseDto.java
@@ -5,8 +5,8 @@ import lombok.Data;
 
 @Data
 public class LoginResponseDto {
-    private String token;
-    private Long userId;
+    private String accessToken;
+    private String userId;
     private String email;
     private String role;
 }
